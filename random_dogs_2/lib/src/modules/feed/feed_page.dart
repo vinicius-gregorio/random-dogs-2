112e12d321fbdb7fceff9cf188dd3af6aabc0fd6
Vinicius Gregorio <vincamgreg@hotmail.com>
2021-08-19 19:31:33 -0300

    feat: add fixed scroll height

--- random_dogs_2/lib/src/modules/feed/feed_page.dart
+++ random_dogs_2/lib/src/modules/feed/feed_page.dart
@@ -3,6 +3,7 @@ import 'package:random_dogs_2/src/modules/feed/domain/entities/dog_response.dart
 import 'package:random_dogs_2/src/modules/feed/domain/usecases/load_feed_usecase.dart';
 import 'package:random_dogs_2/src/modules/feed/feed_controller.dart';
 import 'package:random_dogs_2/src/modules/feed/ui/components/feed_item_component.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({Key? key}) : super(key: key);
@@ -12,7 +13,10 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
-  ScrollController _scrollController = ScrollController();
+  final ItemScrollController _itemScrollController = ItemScrollController();
+  final ItemPositionsListener _itemPositionsListener =
+      ItemPositionsListener.create();
+
   final controller = FeedController();
 
   @override
@@ -26,17 +30,37 @@ class _FeedPageState extends State<FeedPage> {
       var maxHeight = constraints.maxHeight;
       return Container(
         child: FutureBuilder<DogResponse?>(
-            future: controller.getPhotos(LoadFeedParams(numberOfPhotos: 30)),
+            future: controller.getPhotos(LoadFeedParams(numberOfPhotos: 10)),
             builder: (_, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
                 final dogPhotos = snapshot.data!.photo!;
-                return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data?.photo?.length,
+                return ScrollablePositionedList.builder(
+                    itemScrollController: _itemScrollController,
+                    itemPositionsListener: _itemPositionsListener,
+                    itemCount: snapshot.data!.photo!.length,
                     itemBuilder: (__, index) {
-                      return Container(
-                          height: maxHeight,
-                          child: FeedItem(photo: dogPhotos[index]));
+                      return GestureDetector(
+                        onVerticalDragUpdate: (drag) {
+                          if (drag.primaryDelta! < 0) {
+                            _itemScrollController.scrollTo(
+                                index: index + 1,
+                                duration: Duration(milliseconds: 100),
+                                curve: Curves.easeInOutCubic);
+                          } else {
+                            //ensure that index is not negative
+                            if (index - 1 < 0) {
+                              return;
+                            }
+                            _itemScrollController.scrollTo(
+                                index: index - 1,
+                                duration: Duration(milliseconds: 100),
+                                curve: Curves.easeInOutCubic);
+                          }
+                        },
+                        child: Container(
+                            height: maxHeight,
+                            child: FeedItem(photo: dogPhotos[index])),
+                      );
                     });
               } else {
                 return Center(child: CircularProgressIndicator());
