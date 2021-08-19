d7a9fae54176c3a7030a5fa1c1a180a24e25151c
Vinicius Gregorio <vincamgreg@hotmail.com>
2021-08-19 17:56:20 -0300

    refactor: fix image size

--- random_dogs_2/lib/src/modules/feed/feed_page.dart
+++ random_dogs_2/lib/src/modules/feed/feed_page.dart
@@ -22,23 +22,43 @@ class _FeedPageState extends State<FeedPage> {
   final controller = FeedController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder<DogResponse?>(
-          future: controller.getPhotos(LoadFeedParams(numberOfPhotos: 50)),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              final dogPhotos = snapshot.data!.photo!;
-              return Container(
-                child: ListView.builder(
+    return LayoutBuilder(builder: (context, constraints) {
+      var maxHeight = constraints.maxHeight;
+      return Container(
+        child: FutureBuilder<DogResponse?>(
+            future: controller.getPhotos(LoadFeedParams(numberOfPhotos: 30)),
+            builder: (_, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                final dogPhotos = snapshot.data!.photo!;
+                return ListView.builder(
                     itemCount: snapshot.data?.photo?.length,
                     itemBuilder: (__, index) {
-                      return Image.network(dogPhotos[index]);
-                    }),
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          }),
-    );
+                      return Container(
+                          height: maxHeight,
+                          child: FeedItem(photo: dogPhotos[index]));
+                    });
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }),
+      );
+    });
+  }
+}
+
+class FeedItem extends StatelessWidget {
+  final String photo;
+  const FeedItem({
+    Key? key,
+    required this.photo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Image.network(
+      photo,
+      fit: BoxFit.cover,
+    ));
   }
 }
