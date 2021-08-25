536489e4dbdd51681fa6e70f56847cb612d710a2
Vinicius Gregorio <vincamgreg@hotmail.com>
2021-08-25 19:24:29 -0300

    feat: add interaction buttons row

--- random_dogs_2/lib/src/modules/feed/ui/components/scrollable_item_component.dart
+++ random_dogs_2/lib/src/modules/feed/ui/components/scrollable_item_component.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:random_dogs_2/src/shared/components/lottie_animated_icon.dart';
 import 'package:random_dogs_2/src/theme/app_animations.dart';
+import 'package:random_dogs_2/src/theme/app_colors.dart';
 
 import '../../feed_controller.dart';
 import 'feed_item_component.dart';
@@ -24,6 +25,46 @@ class ScrollableItem extends StatelessWidget {
       children: [
         Container(height: maxHeight, child: FeedItem(photo: dogPhotos[index])),
         Positioned(
+            bottom: maxHeight * 0.1,
+            right: maxHeight * 0.01,
+            child: Container(
+              width: 50,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        print('pressed Share');
+                      },
+                      icon: Icon(
+                        Icons.favorite_outline,
+                        size: 30,
+                        color: AppColors.icon,
+                      )),
+                  Padding(padding: EdgeInsets.only(top: 5)),
+                  IconButton(
+                      onPressed: () {
+                        print('pressed Share');
+                      },
+                      icon: Icon(
+                        Icons.copy,
+                        size: 30,
+                        color: AppColors.icon,
+                      )),
+                  Padding(padding: EdgeInsets.only(top: 5)),
+                  IconButton(
+                      onPressed: () {
+                        print('pressed Share');
+                      },
+                      icon: Icon(
+                        Icons.send_outlined,
+                        size: 30,
+                        color: AppColors.icon,
+                      )),
+                ],
+              ),
+            )),
+        Positioned(
             bottom: maxHeight * 0.02,
             left: 0,
             right: 0,
@@ -33,7 +74,8 @@ class ScrollableItem extends StatelessWidget {
               },
               child: LottieAnimatedIcon(
                 iconUrl: LottieAnimations.arrowDownPump,
-                height: 70,
+                height: maxHeight * 0.08,
+                width: 30,
               ),
             )),
       ],
