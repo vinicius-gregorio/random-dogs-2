cd5ca9b1d084c89c18f7142c63912852b2f4876b
Vinicius Gregorio <vincamgreg@hotmail.com>
2021-08-29 16:49:51 -0300

    refactor: move http to utils folder

--- random_dogs_2/lib/src/shared/utils/domain/errors/convert_errors.dart
+++
@@ -1,8 +0,0 @@
-class ConvertError implements Exception {
-  final String message;
-
-  ConvertError({required this.message});
-
-  @override
-  String toString() => 'ConvertError(message: $message)';
-}
