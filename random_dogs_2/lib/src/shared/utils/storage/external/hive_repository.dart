3758b688b31b54dd3806281744fed247c0b0656d
vinicius-gregorio <vincamgreg@hotmail.com>
2021-10-19 20:22:11 -0300

    feat: add get Hive favorites

--- random_dogs_2/lib/src/shared/utils/storage/external/hive_repository.dart
+++ random_dogs_2/lib/src/shared/utils/storage/external/hive_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:random_dogs_2/src/shared/utils/storage/domain/repositories/storage_repository.dart';
@@ -13,9 +14,17 @@ class HiveRepository implements IStorageRepository {
   }
 
   @override
-  Future read() {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future read() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+
+    if (!Hive.isBoxOpen('Favorites')) {
+      Hive.init(appDocPath);
+    }
+    var box = Hive.box('Favorites');
+    var values = box.toMap();
+    print(values);
+    return values;
   }
 
   @override
@@ -28,7 +37,6 @@ class HiveRepository implements IStorageRepository {
 
     var box = await Hive.openBox('Favorites');
     await box.add(data);
-    print(box.get(data));
   }
 
   @override
