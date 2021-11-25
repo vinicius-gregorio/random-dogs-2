f811aab495be84940c166fc3f6fb7d5dce962c0f
vinicius-gregorio <vincamgreg@hotmail.com>
2021-11-24 21:20:47 -0300

    feat: add buttons to favorites

--- random_dogs_2/lib/src/modules/favorites/ui/components/grid_item.dart
+++ random_dogs_2/lib/src/modules/favorites/ui/components/grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_dogs_2/src/theme/app_colors.dart';
 
 class GridItem extends StatelessWidget {
   final imageUrl;
@@ -7,10 +8,41 @@ class GridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350,
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
+      child: Column(
+        children: [
+          Container(
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      print('pressed unfavorite');
+                    },
+                    icon: Icon(
+                      Icons.favorite_outline,
+                      size: 30,
+                      color: AppColors.icon,
+                    )),
+                IconButton(
+                    onPressed: () {
+                      print('pressed share');
+                    },
+                    icon: Icon(
+                      Icons.send_outlined,
+                      size: 30,
+                      color: AppColors.icon,
+                    )),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
